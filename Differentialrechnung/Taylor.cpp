68dc0cbdc90ea9c38a12db4d5fca0cdfa7c3b023
Julian L. Avila <julianlavilam@gmail.com>
2024-03-23 17:55:34 -0500

    Add saveDataToFile function

--- Differentialrechnung/Taylor.cpp
+++ Differentialrechnung/Taylor.cpp
@@ -3,14 +3,14 @@
 #include<cstdlib>
 #include<cmath>
 
-const double InitialX = 0.0;
-const double InitialY = 1.0;
-const double StepSize = 0.001;
+const double InitialX = 0.0; // Initial Condition x
+const double InitialY = 1.0; // Inital Condition y
+const double StepSize = 0.001; // Stepsize 1 (h) for Taylor Method
+const double StepSize2 = 0.01; // Stepsize 2 (h) for Taylor Method
 
-double StepSize2 = StepSize * StepSize;
-double StepSize3 = StepSize2 * StepSize;
-
-double analitcySolution(double x) {
+// Analytic solution of differenital equation
+// y' = (1 + x)y² / 2
+double analyticSolution(double x) {
 	return -1.0 / ( (x * x) * 0.25 + x * 0.5 - 1);
 }
 
@@ -27,20 +27,30 @@ double thirdDerivative(double x, double y) {
 }
 
 double taylorSolution(double x, double y, double stepSize) {
-	return x + stepSize * firstDerivative(x, y) + 0.5 * StepSize2 * secondDerivative(x, y) + (1.0 / 6) * StepSize3 * thirdDerivative(x, y);
+	double stepSizeSquare = stepSize * stepSize;
+	double stepSizeCube = stepSizeSquare * stepSize;
+	return x + stepSize * firstDerivative(x, y) + 0.5 * stepSizeSquare * secondDerivative(x, y) + (1.0 / 6) * stepSizeCube * thirdDerivative(x, y);
 }
 
-double sumTaylorSolution(double initalx, double initialy, double stepSize, double upperLimit) {
+void  saveDataToFile(double initalx, double initialy, double stepSize, double upperLimit, double lowerLimit, const std::string& filename) {
 	double x = initalx;
-	double y = initialy;
-	for (int i = 1; i < upperLimit; ++i) {
-		x += stepSize;
-		y += taylorSolution(x, y, stepSize);
+	double yTaylorh = initialy;
+	double y = analyticSolution(x);
+	double N = (upperLimit - lowerLimit) / stepSize;
 
+	std::ofstream datafile(filename);
+
+	for (int i = 1; i < N; ++i) {
+		datafile << x << '\t' << y << '\t' << yTaylorh << std::endl;
+		yTaylorh += taylorSolution(x, yTaylorh, stepSize);
+		y = analyticSolution(x);
+		x += stepSize;
 	}
-	return y;
+	datafile.close();
 }
 
 int main() {
-	
+	saveDataToFile(InitialX, InitialY, StepSize, 1.0, 0.0, "results-h0.001.dat");	
+
+	return 0;
 }
