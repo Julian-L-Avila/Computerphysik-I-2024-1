74b127ddeeba1604fa93b6d6510af31dd37371f8
Laurah134 <laurahmy134@gmail.com>
2024-03-29 19:22:22 -0500

    analitic solution

--- Two-Body-Problem/P.cpp
+++ Two-Body-Problem/P.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 #include<cmath>
 #include<fstream>
-#include<cmath>
+
+//funcion analitica
+double analitica(double theta){
+    double m0;
+    double m1;
+    double e;
+    double R;
+    double v;
+    double G=6.67430*pow(10,-11);
+    double mu=(m0*m1)/(m0+m1);
+    double L=mu*R*v;
+    double c=G*m0*m1;
+    double E=((e*e-1)*mu*c*c)/2*L*L;
+    return ((L*L)/mu*c)/(1+cos(theta)*sqrt(1+(2*E*L*L)/(mu*c*c)));
+}
+
 
 
 
