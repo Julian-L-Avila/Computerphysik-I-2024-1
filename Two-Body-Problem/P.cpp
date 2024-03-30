5f83f7bc02c110265505bfb9c7d1409fda023d6f
Laurah134 <laurahmy134@gmail.com>
2024-03-29 19:54:17 -0500

    data of analitic function

--- Two-Body-Problem/P.cpp
+++ Two-Body-Problem/P.cpp
@@ -17,6 +17,21 @@ double analitic(double theta){
     return ((L*L)/mu*c)/(1+cos(theta)*sqrt(1+(2*E*L*L)/(mu*c*c)));
 }
 
+int main()
+{
+    std::ofstream datafile("analitic_funtion.dat");
+    double pi=M_PI;
+
+    for(double theta=0.0;theta<=pi; theta+=0.00001)
+    {
+        datafile<<theta<<" "<<analitic(theta)<<std::endl;
+        datafile<<theta<<" "<<0.0<<std::endl;
+        datafile<<std::endl;
+    }
+
+    datafile.close();
+}
+
 
 
 
