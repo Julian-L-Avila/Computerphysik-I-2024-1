055781707a7e9d094690ebe3761a6b9a81502020
Laurah134 <laurahmy134@gmail.com>
2024-03-29 22:58:09 -0500

    grafic  of analitic function

--- Two-Body-Problem/P.cpp
+++ Two-Body-Problem/P.cpp
@@ -4,32 +4,53 @@
 
 //funcion analitica
 double analitic(double theta){
-    double m0;
-    double m1;
-    double e;
-    double R;
-    double v;
+    double m0=5.9736*pow(10,24);
+    double m1=1.9891*pow(10,30);
+    double e=0.016711233;
+    double R=149598261000;
+    double v=29780;
     double G=6.67430*pow(10,-11);
     double mu=(m0*m1)/(m0+m1);
     double L=mu*R*v;
     double c=G*m0*m1;
-    double E=((e*e-1)*mu*c*c)/2*L*L;
+    double E=((e*e-1)*mu*c*c)/(2*L*L);
     return ((L*L)/mu*c)/(1+cos(theta)*sqrt(1+(2*E*L*L)/(mu*c*c)));
 }
 
 int main()
 {
+    //Dat of analitic function
     std::ofstream datafile("analitic_funtion.dat");
     double pi=M_PI;
 
-    for(double theta=0.0;theta<=pi; theta+=0.00001)
+    for(double theta=0.0;theta<=2*3.14; theta+=0.00001)
     {
-        datafile<<theta<<" "<<analitic(theta)<<std::endl;
-        datafile<<theta<<" "<<0.0<<std::endl;
-        datafile<<std::endl;
+        double r_a=analitic(theta);
+        datafile<<theta<<" "<<r_a<<std::endl;
     }
 
     datafile.close();
+
+
+    //Grafic of analitic funtion
+    std::ofstream scriptFile("analitic_funtion.gp");
+    scriptFile<<"set term png\n";
+    scriptFile<<"set output 'analitic_funtion.png'\n";
+    scriptFile<<"set polar\n";
+    scriptFile<<"set size square\n";
+    scriptFile<<"set grid polar\n";
+    scriptFile<<"x(r,theta)=r*cos(theta)\n";
+    scriptFile<<"y(r,theta)=r*sin(theta)\n";
+    scriptFile<<"set xlabel 'x'\n";    
+    scriptFile<<"set ylabel 'y'\n";
+    scriptFile<<"plot 'analitic_funtion.dat' (x($1:$2)):(y($1:$2)) with lines title 'Datos'\n";
+    scriptFile.close();
+
+    system("analitic_funtion.gp");
+
+    return 0;
+
+
 }
 
 
