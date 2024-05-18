7cfa3f7ef245ab3247e912ce76991f537a9d7d95
Julian L Avila <julianlavilam@gmail.com>
2024-05-18 13:17:11 -0500

    Add system of differential equations for linear pendulum

--- Pendulum/pendulum.cpp
+++ Pendulum/pendulum.cpp
@@ -5,6 +5,38 @@
  */
 
 #include <iostream>
+#include <cmath>
+
+double initial_angle, initial_velocity, natural_frequency,
+			natural_frequency_square;
 
 int main() {
+}
+
+// Pendulum (Linear)
+// TODO
+
+long double AngleDerivativeLinear(long double& velocity) {
+	return velocity;
+}
+
+long double VelocityDerivativeLinear(long double& angle,
+		double& natural_frequency_square) {
+
+	return - natural_frequency_square * angle;
+}
+
+long double AnalyticAngleLinear(double t) {
+	double A = initial_angle;
+	long double B = initial_velocity / natural_frequency;
+
+	return A * std::cos(natural_frequency * t) + B * std::sin(natural_frequency * t);
+}
+
+long double AnalyticVelocityLinear(double t) {
+	double A = initial_angle;
+	long double B = initial_velocity / natural_frequency;
+
+	return B * natural_frequency * std::cos(natural_frequency * t) -
+		A * natural_frequency * std::sin(natural_frequency * t);
 }
