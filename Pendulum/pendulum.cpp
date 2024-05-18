bc58b112bbae223192ee7bff90baad12833bdc61
Julian L Avila <julianlavilam@gmail.com>
2024-05-18 11:33:28 -0500

    Add Pendulum directory for project and main cpp script

---
+++ Pendulum/pendulum.cpp
@@ -0,0 +1,10 @@
+/* Pendulum and Double Pendulum Simulation for Computational Physics 2nd Exam
+ * UD FCJ - FCMN - Physics
+ * Made by:
+ * - Julian Avila - 20212107030
+ */
+
+#include <iostream>
+
+int main() {
+}
